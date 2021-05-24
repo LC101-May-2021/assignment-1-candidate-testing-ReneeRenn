6c1931a149ff2639e8056df9f86f7cc3867ea8d3
Renee Renn <>
2021-05-24 00:59:25 +0000

    Part one

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,46 +1,57 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+
+let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+
+let question = ("1) Who was the first American woman in space?\nYour Answer: ");
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
-
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+candidateName = input.question("What is your name? ");
+console.log("Candidate Name:"+" "+ candidateName);
 
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  // TODO 1.2b: Ask candidate the question and assign the response as 
+candidateAnswer = input.question(question);
+console.log("Correct Answer: "+ correctAnswer);
+console.log();
 
 
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+let grade = (1/1) * 100;
+if (candidateAnswer === "Sally Ride"){
+  console.log("Your answer is correct");
+} else {
+  console.log("Your answer is incorrect");
 
-
-  let grade;
-  
+}
 
   return grade;
+
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Welcome to your evaluation quiz,"+" "+ candidateName);
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
+
 }
 
 // Don't write any code below this line //
