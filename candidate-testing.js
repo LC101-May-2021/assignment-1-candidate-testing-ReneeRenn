e5132ead197de80e0068b5724f839f9d9bf66974
Renee Renn <>
2021-06-03 18:48:24 +0000

    added some of section 2 as a comment

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,6 +7,10 @@ let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
+//let questions = ["Whow was the first American Woman in space?", "True or False: 5 kilometer == 5000 meters?","(5+3)/2*10 = ?", "Given the array [8,'orbit', 'Trajectory', 45]. What entry is at index 2?", "What is the maximum crew size for the ISS?"];
+
+//let answers = ["Sally Ride", true, 40, "Trajectory", 3];
+
 let question = ("1) Who was the first American woman in space?\nYour Answer: ");
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
